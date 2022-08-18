fdb9bf68f803085d8723ad1ffe39c256114f3973
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2022-08-19 01:30:28 +0900

    Updated main.py

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Command
 
 ```
+docker run --volume ${PWD}:/opt/learn_jinja2 -ti python:3 bash
+
+cd /opt/learn_jinja2
 python -m venv venv/
 source venv/bin/activate
 
--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 import jinja2
 
 class Main:
-    def __init__(self):
-        pass
+    parameters = [
+            {"name": "name01", "value": "value01"},
+            {"name": "name02", "value": "value02"}
+    ]
 
     def exec(self):
-        environment = jinja2.Environment()
-        template = environment.from_string("Hello, {{ name }}")
-        result = template.render(name="World")
-        print(result)
+        environment = jinja2.Environment(loader=FileSystemLoader("."))
+        environmen.get_template("template.j2")
+        template.render()
 
 if __name__ == "__main__":
     Main().exec()
