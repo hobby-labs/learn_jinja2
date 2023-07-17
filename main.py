b6335a0200a699243ce2b8f60e3750da0a9d3783
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2023-07-17 12:32:53 +0900

    Updated test template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import jinja2
+from jinja2 import Environment, FileSystemLoader
 
 class Main:
     parameters = [
@@ -9,8 +10,8 @@ class Main:
 
     def exec(self):
         environment = jinja2.Environment(loader=FileSystemLoader("."))
-        environmen.get_template("template.j2")
-        template.render()
+        template = environment.get_template("template.j2")
+        print(template.render(parameters = self.parameters))
 
 if __name__ == "__main__":
     Main().exec()
